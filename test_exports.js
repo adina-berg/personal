cf986e46ac3f4d571c9474f070979648c74baf40
adina-berg <adina.berg@live.se>
2024-09-23 16:17:15 +0200

    Create test_exports.js
    Add test_exports.js file.

---
+++ test_exports.js
@@ -0,0 +1,11 @@
+/*
+* A nice constant number value.
+*/
+export const A_NUMBER = 1234;
+
+/**
+* Adds two numbers and returns the sum.
+*/
+export function add(first, second) {
+  return first + second;
+}
